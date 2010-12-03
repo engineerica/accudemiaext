d012e9149656f8c9db28206cf8106abc2771b619
jancic@gmail.com <jancic@gmail.com>
2010-12-03 15:46:06 +0000

    Initial version of the SDK and a simple example plugin

---
+++ plugins/ExamplePlugin/ExamplePlugin/PluginLoader.cs
@@ -0,0 +1,15 @@
+﻿namespace ExamplePlugin
+{
+	using System;
+	using System.Windows.Forms;
+	using AccudemiaDataX.Core;
+
+	public class PluginLoader : Plugin
+	{
+		public override void Load()
+		{
+			MessageBox.Show("Loading plugin...");
+			Bind<IDataSource>().To<XmlDataSource>();
+		}
+	}
+}
