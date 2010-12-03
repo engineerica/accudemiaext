9c2c9fff909d419eb586489d4faf4875db02174c
jancic@gmail.com <jancic@gmail.com@82778f16-cb00-9be5-2e83-e9dbbff6b5b3>
2010-12-03 19:45:52 +0000

    Fixed error while uploading the entity fields

--- trunk/plugins/ExamplePlugin/ExamplePlugin/PluginLoader.cs
+++ trunk/plugins/ExamplePlugin/ExamplePlugin/PluginLoader.cs
@@ -1,15 +1,11 @@
 ﻿namespace ExamplePlugin
 {
-	using System;
-	using System.Windows.Forms;
 	using AccudemiaDataX.Core;
 
 	public class PluginLoader : Plugin
 	{
 		public override void Load()
 		{
-			MessageBox.Show("Hi, I'm loading the plugins...");
-
 			// Add here all the data sources you want.
 			Bind<IDataSource>().To<MockDataSource>();
 		}
